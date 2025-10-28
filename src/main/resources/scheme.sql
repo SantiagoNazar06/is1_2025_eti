4e6f0dae4f1c6fcae3079b4ebb17585c0310a384
cgalean0 <cngaleano56@gmail.com>
2025-10-28 11:24:50 -0300

    Feat: Agregué las tablas Person y Teacher, falta implementar Students

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -7,3 +7,38 @@ CREATE TABLE users (
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
 );
+
+-- Elimina la tabla 'persons' si ya existe para asegurar un inicio limpio
+DROP TABLE IF EXISTS persons;
+
+-- Crea la tabla 'persons' con los campos originales, adaptados para SQLite
+CREATE TABLE persons (
+    id INTEGER PRIMARY KEY,
+    dni INTEGER NOT NULL UNIQUE -- Clave primaria autoincremental para SQLite
+    firstName TEXT NOT NULL,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
+    lastName TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+
+-- Elimina la tabla 'teacher' si ya existe para asegurar un inicio limpio
+DROP TABLE IF EXISTS teacher;
+
+-- Crea la tabla 'teacher' con los campos originales, adaptados para SQLite
+CREATE TABLE teacher (
+    id_persona INTEGER PRIMARY KEY,
+    nroLegajo INTEGER NOT NULL UNIQUE,
+    FOREIGN KEY id_persona REFERENCES person(id)
+);
+
+
+-- Elimina la tabla 'student' si ya existe para asegurar un inicio limpio
+DROP TABLE IF EXISTS student;
+
+-- Crea la tabla 'student' con los campos originales, adaptados para SQLite
+CREATE TABLE student (
+    id INTEGER PRIMARY KEY,
+    typeStudent ,
+    CONSTRAINT ch_pk_student FOREIGN KEY id REFERENCES person(id)
+);
